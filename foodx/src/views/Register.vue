7d41d426b35cf0ff26ec4a4d21f6fdb612cd39ef
ninamacuka <>
2021-01-30 18:37:50 +0100

    login i register stranica
    kreiranje login i register stranice

---
+++ foodx/src/views/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-9">
+          <form>
+            <div class="form-group">
+              <label for="Email">Email:</label>
+              <input type="Email" class="form-control" placeholder="Email" />
+            </div>
+            <div class="form-group">
+              <label for="password">Password:</label>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <label for="password2">Confirm Password:</label>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Confirm Password"
+              />
+            </div>
+            <button class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
+    </div>
+  </div>
+</template>
