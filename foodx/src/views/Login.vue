f1d800f6a5e7bfa0348cd11fdd19e015e24627c4
ninamacuka <>
2021-01-31 17:29:31 +0100

    login
    logiranje sa firebaseom

--- foodx/src/views/Login.vue
+++ foodx/src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
+  <div class="about">
+    <h1>This is an login page</h1>
     <div class="container">
       <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm">
+        <div class="col-3"></div>
+        <div class="col-6">
           <form>
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
@@ -14,6 +14,7 @@
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
+                v-model="email"
               />
             </div>
             <div class="form-group">
@@ -23,13 +24,45 @@
                 class="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
+                v-model="password"
               />
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button v-on:click.prevent="login" class="btn btn-primary">
+              Submit
+            </button>
           </form>
         </div>
-        <div class="col-sm"></div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { firebase } from "@/firebase";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    login: function () {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          () => {
+            this.$router.replace({ name: "Home" });
+          },
+          (err) => {
+            alert("oops" + err.message);
+          }
+        );
+    },
+  },
+};
+</script>
